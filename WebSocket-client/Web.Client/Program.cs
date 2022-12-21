807fffac17d89a0824f4fb5f4f3a8487c670f1d3
Yurii-Polischukkk <113755012+Yurii-Polischukkk@users.noreply.github.com>
2022-12-21 23:11:06 +0200

    wcf project

---
+++ WebSocket-client/Web.Client/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Web.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("press enter to count...... ");
+            Console.ReadLine();
+
+            using (ClientWebSocket client = new ClientWebSocket())
+
+            {
+                Uri serviceUri = new Uri("ws://localhost:5000/send");
+                var cTs = new CancellationTokenSource();
+                cTs.CancelAfter(TimeSpan.FromSeconds(120));
+                try
+                {
+                    await client.ConnectAsync(serviceUri, cTs.Token);
+                    var n = 0;
+                    while (client.State == WebSocketState.Open)
+                    {
+                        Console.WriteLine("enter message to send");
+                        string message = Console.ReadLine();
+                        if (!string.IsNullOrEmpty(message))
+                        {
+                            ArraySegment<byte> byteToSend = new ArraySegment<byte>(Encoding.UTF8.GetBytes(message));
+                            await client.SendAsync(byteToSend, WebSocketMessageType.Text, true, cTs.Token);
+                            var responseBuffer = new byte[1024];
+                            var offset = 0;
+                            var packet = 1024;
+                            while (true)
+                            {
+                                ArraySegment<byte> byteRecieved = new ArraySegment<byte>(responseBuffer, offset, packet);
+                                WebSocketReceiveResult response = await client.ReceiveAsync(byteRecieved, cTs.Token);
+                                var responseMessage = Encoding.UTF8.GetString(responseBuffer, offset, response.Count);
+                                Console.WriteLine(responseMessage);
+                                if (response.EndOfMessage)
+                                    break; 
+                            }
+                        }
+                    }
+                }
+                catch (WebSocketException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                }
+            Console.ReadLine();
+            }
+    }
+}
